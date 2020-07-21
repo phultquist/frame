c9246b3f1193300b37b3b5884ed97e8a988cb4da
phultquist <phultquist@imsa.edu>
2020-07-21 01:26:22 -0500

    sixteen

--- display.py
+++ display.py
@@ -12,7 +12,7 @@ pixels = neopixel.NeoPixel(board.D18, 256)
 # pixels.fill((238, 37, 65))
 
 j = 0
-step = 10
+step = 16
 while j < len(pixels):
     pixels[j - step:j] = mbdtf[j - step:j]
     j += step
