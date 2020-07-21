4c9606de6ce67695b046386b8e7113129bec82bf
phultquist <phultquist@imsa.edu>
2020-07-21 01:50:52 -0500

    one percent

--- display.py
+++ display.py
@@ -5,7 +5,7 @@ from mbdtf import *
 
 seconds = 0
 
-pixels = neopixel.NeoPixel(board.D18, 256, brightness = 0.05)
+pixels = neopixel.NeoPixel(board.D18, 256, brightness = 0.01)
 
 # pixels = mbdtf
 
