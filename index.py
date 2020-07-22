2a5207fd8bfe7a45bd3bcf0807d1832f133dd254
phultquist <phultquist@imsa.edu>
2020-07-21 21:53:12 -0500

    index.py updated with error correction

--- index.py
+++ index.py
@@ -6,17 +6,18 @@ from scipy.io.wavfile import write
 import PIL.Image
 from io import BytesIO
 import numpy as np
+from datetime import datetime
+startTime = datetime.now()
 
 import board
 import neopixel
-# import sys
 
 
 ####################
 ### Record Audio ###
 ####################
 
-duration = 3  # seconds
+duration = 4  # seconds
 fs = 44100
 
 recorded = sd.rec(int(duration * fs), samplerate=fs, channels=1)
@@ -43,18 +44,23 @@ data = {
 result = requests.post('https://api.audd.io/', data=data)
 
 response = json.loads(result.text)
-print(response['result']['title'])
-print(response['result']['artist'])
-imgurl = response['result']['spotify']['album']['images'][0]['url']
 
+try:
+    print(response['result']['title'])
+    print(response['result']['artist'])
+    imgurl = response['result']['spotify']['album']['images'][0]['url']
+except:
+    print('### error 1 ###')
+    print(response)
+    imgurl = 'https://i.ibb.co/Q85tMW2/e1.png' # error 1
 
 
 ################################
 ### Get Image and Manipulate ###
 ################################
 
-response = requests.get(imgurl)
-img = PIL.Image.open(BytesIO(response.content))
+imgresp = requests.get(imgurl)
+img = PIL.Image.open(BytesIO(imgresp.content))
 
 img = img.resize((16, 16))
 img.show()
@@ -67,9 +73,12 @@ for ri in range(len(imgpx)):
         finalpx.append((imgpx[ri][ci][0], imgpx[ri][ci][1], imgpx[ri][ci][2]))
 
 
+print('Total Time: ' + str(datetime.now() - startTime))
+
 #####################
 ### Update Pixels ###
 #####################
+
 
 pixels = neopixel.NeoPixel(board.D18, 256, brightness = 0.1)
 
