94134f8ca0b308ecc8544244c27f13106df9cf1b
phultquist <phultquist@imsa.edu>
2020-08-09 22:50:09 -0500

    removed unnecessary code

--- testscripts/lightsensor.py
+++ testscripts/lightsensor.py
@@ -34,14 +34,5 @@ while True:
     # These are unsigned, the higher the number the more light of that type.
     # There are no units like lux.
     # Infrared levels range from 0-65535 (16-bit)
-    '''
-    infrared = sensor.infrared
-    print("Infrared light: {0}".format(infrared))
-    # Visible-only levels range from 0-2147483647 (32-bit)
-    visible = sensor.visible
-    print("Visible light: {0}".format(visible))
-    # Full spectrum (visible + IR) also range from 0-2147483647 (32-bit)
-    full_spectrum = sensor.full_spectrum
-    print("Full spectrum (IR + visible) light: {0}".format(full_spectrum))
-    '''
+
     time.sleep(1.0)
