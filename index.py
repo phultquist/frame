d9fab25e86e3bc0d9017d94baed91430f68dc18c
amhenikoff <addison@henikoff.com>
2020-08-11 00:13:05 -0500

    Update index.py

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ def get_argument(index):
 brt = 0.07
 
 if get_argument(1) != False and type(get_argument(1)) is int:
-    brt = int(sys.argv[1]) / 100
+    brt = int(get_argument(1)) / 100
 print(brt)
 print(get_argument(1))
 
