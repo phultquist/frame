2a69d9ddbd2df709f4b3450c89705f9c1a3cacbc
phultquist <phultquist@imsa.edu>
2020-08-15 12:55:59 -0500

    adjust

--- testscripts/fade.py
+++ testscripts/fade.py
@@ -7,6 +7,10 @@ v = 200
 mult = 1
 while True:
     if v > 255 or v < 0:
+        if v > 255:
+            v = 255
+        else:
+            v = 0
         mult *= -1
     
     # for the life of me, i have no idea why this has to be a loop. i tried pixels = mbdtf and every time it showed funky colors, so here we are
