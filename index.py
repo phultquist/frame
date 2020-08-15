f8f8486214113176d16a76456c24d5ec524a8308
phultquist <phultquist@imsa.edu>
2020-08-15 01:25:40 -0500

    13

--- index.py
+++ index.py
@@ -25,7 +25,7 @@ if get_argument(1) != None and get_argument(1) != "test" and get_argument(1) != 
 if get_argument(1) == 'auto':
     import light
     import math
-    brt = math.sqrt(light.lux()) / 10
+    brt = math.sqrt(light.lux()) / 13
     print('Set brightness automatically to '+brt)
 
 if get_argument(2) == 'noserver':
