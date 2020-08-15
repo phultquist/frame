eda8103e5729248fd250388ec3e6a2184e421f5d
phultquist <phultquist@imsa.edu>
2020-08-15 12:34:55 -0500

    uhh

--- testscripts/fade.py
+++ testscripts/fade.py
@@ -8,13 +8,10 @@ mult = 1
 while True:
     if v > 255 or v < 0:
         mult *= -1
-
-    j = 0
-    step = 256
-
+    
     # for the life of me, i have no idea why this has to be a loop. i tried pixels = mbdtf and every time it showed funky colors, so here we are
     print(v)
-    pixels[0:255] = (v, v, v)
+    pixels[0 - 256:0] = (v, v, v)
     
     v += mult * 3
 
