8bc7d42d7411f05ea5bb4eb1e5c2f231a5696991
phultquist <phultquist@imsa.edu>
2020-08-15 13:09:20 -0500

    swipe.py

---
+++ testscripts/swipe.py
@@ -0,0 +1,14 @@
+import time
+import board
+import neopixel
+pixels = neopixel.NeoPixel(board.D12, 256, brightness=0.1, auto_write=True)
+
+v = 0
+while True:
+    if v > 15:
+        v = 0
+    # for the life of me, i have no idea why this has to be a loop. i tried pixels = mbdtf and every time it showed funky colors, so here we are
+    pixels[v] = (200, 200, 200)
+    pixels.show()
+    v += 1
+
