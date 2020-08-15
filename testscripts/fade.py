03fafb23fb6de3e480f1db008b5c90eb00345a07
phultquist <phultquist@imsa.edu>
2020-08-15 12:38:23 -0500

    add other?

--- testscripts/fade.py
+++ testscripts/fade.py
@@ -3,7 +3,7 @@ import board
 import neopixel
 pixels = neopixel.NeoPixel(board.D12, 256, brightness=0.1)
 
-v = 0
+v = 1
 mult = 1
 while True:
     if v > 255 or v < 0:
@@ -11,7 +11,7 @@ while True:
     
     # for the life of me, i have no idea why this has to be a loop. i tried pixels = mbdtf and every time it showed funky colors, so here we are
     print(v)
-    pixels[1:100] = (v, v, v)
+    pixels[1:100] = (v, v, v, 1)
     
     v += mult * 3
 
