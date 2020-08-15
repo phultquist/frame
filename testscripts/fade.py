beae2fa70b1b1aa9e20dd885298bc8c0852b8a39
phultquist <phultquist@imsa.edu>
2020-08-15 11:29:55 -0500

    test script

---
+++ testscripts/fade.py
@@ -0,0 +1,14 @@
+import time
+import board
+import neopixel
+pixels = neopixel.NeoPixel(board.D12, 256, brightness=0.1)
+
+v = 0
+mult = 1
+while True:
+    if v > 255 or v < 0:
+        mult *= -1
+
+    pixels.fill((v, v, v))
+    v += mult * 1
+
