1dbe4ea8b68b8ed3501740cb33f394247e14a548
phultquist <phultquist@imsa.edu>
2020-08-19 00:03:36 -0500

    red to green

--- testscripts/fade.py
+++ testscripts/fade.py
@@ -5,7 +5,7 @@ pixels = neopixel.NeoPixel(board.D12, 256, brightness=0.1, auto_write=False)
 
 v = 200
 mult = 1
-color = [0, 0, 0]
+color = [0, 255, 0]
 newcolor= [255, 0, 0]
 steps = 28
 stepcount = 0
