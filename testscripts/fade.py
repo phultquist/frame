fcca307c19c1367dabaa3eab50739d93881061e2
phultquist <phultquist@imsa.edu>
2020-08-18 23:59:48 -0500

    red

--- testscripts/fade.py
+++ testscripts/fade.py
@@ -5,18 +5,31 @@ pixels = neopixel.NeoPixel(board.D12, 256, brightness=0.1, auto_write=False)
 
 v = 200
 mult = 1
+color = [0, 0, 0]
+newcolor= [255, 0, 0]
+steps = 28
+stepcount = 0
+
 while True:
-    if v > 255 or v < 0:
-        if v > 255:
-            v = 255
-        else:
-            v = 0
-        mult *= -1
+    if stepcount > steps:
+        stepcount = 0
     
     # for the life of me, i have no idea why this has to be a loop. i tried pixels = mbdtf and every time it showed funky colors, so here we are
+    updated = [0,0,0]
+    for j in range(3):
+        updated[j] = calc_pixel(color[j], newcolor[j], stepcount)
+
     for i in range(256):
-        pixels[i] = (v, v, v)
+        pixels[i] = (updated[0], updated[1], updated[2])
     pixels.show()
     
-    v += mult * 9
+    stepcount += 1
 
+def calc_pixel(old, new, stepno):
+    p = ((new - old)/steps) * stepno + old
+    if p > 255:
+        p = 255
+    if p < 0:
+        p = 0
+    return p
+    
