7fa7c3f7f6d6572fedb7697698c9ff0966f883f0
phultquist <phultquist@imsa.edu>
2020-08-27 11:00:56 -0500

    new

--- ip.py
+++ ip.py
@@ -3,4 +3,12 @@ import socket
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 
+def get_ip_address():
+    ip_address = '';
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
+
 print(ip)
