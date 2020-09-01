58645ce53e361a413b713e910729845b242f41f2
phultquist <phultquist@imsa.edu>
2020-09-01 00:12:48 -0500

    ip

--- testspotify.py
+++ testspotify.py
@@ -1,5 +1,5 @@
-import spotify
-print(spotify.song())
+# import spotify
+# print(spotify.song())
 
 import spotipy
 import os
@@ -7,14 +7,15 @@ from spotipy.oauth2 import SpotifyOAuth
 from secrets import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, username
 import ip
 
-print(ip)
+print(ip.ip)
+# quit()
 
 os.environ["SPOTIPY_CLIENT_ID"] = SPOTIPY_CLIENT_ID
 os.environ["SPOTIPY_CLIENT_SECRET"] = SPOTIPY_CLIENT_SECRET
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                client_secret=SPOTIPY_CLIENT_SECRET,
-                                               redirect_uri="http://"+ip+":3000/",
+                                               redirect_uri="http://"+str(ip.ip)+":3000/",
                                                scope="user-library-read",
                                                username="jdiaalhsw8zdhpcdv391bj5we",
                                                show_dialog=True,
