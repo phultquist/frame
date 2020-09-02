1efd68c7b2cdc7741a56423a7bb99bea9d873361
phultquist <phultquist@imsa.edu>
2020-09-02 01:03:08 -0500

    catch if json error

--- settings.py
+++ settings.py
@@ -3,11 +3,17 @@ import json
 
 # cur_path = os.path.dirname(__file__)
 # new_path = os.path.relpath('./')
+parsed = ''
 def get():
+    global parsed
     f = open("./mobile/server/settings.json", "r")
     content = f.read()
     # print(content)
-    parsed = json.loads(content)
+    try:
+        parsed = json.loads(content)
+    except:
+        pass
+    
     # print(parsed["brightness"])
     f.close()
     return parsed
