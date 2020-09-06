71eda6fd074385bd01bd9fbe394c515adf2a7452
phultquist <phultquist@imsa.edu>
2020-09-06 14:20:43 -0500

    delete change display

---
+++ wifi-connect/get.sh
@@ -0,0 +1,3 @@
+#this is just a test file; not functional
+exit 0
+bash <(curl -L https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh)
---
+++ wifi-connect/start.sh
@@ -0,0 +1,5 @@
+sudo wifi-connect --portal-ssid ssid Frame
+
+
+# once custom ui is made:
+# sudo wifi-connect --portal-ssid Frame --ui-directory something
