d5e0e6b6b486e4a44191f0930a6a4eaacc98bb72
phultquist <phultquist@imsa.edu>
2020-09-16 01:23:02 -0500

    clock

---
+++ src/clock.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+import PIL.Image
+import numpy as np
+
+
+def get_image_ref(style, digit):
+    return 'assets/clock/'+style+'-'+str(digit)+'.png'
+
+
+def get_image(src):
+    img = PIL.Image.open(src)
+    # img.show()
+    return img
+
+
+# ref = get_image(get_image_ref('modern', 0))
+# print(ref)
+all_images = []
+
+for i in range(10):
+    ref = get_image_ref('modern', i)
+    # get_image(i)
+    all_images.append(get_image(ref))
+
+to_display = datetime.now().strftime("%H%M")
+
+to_display = str(to_display)
+if len(to_display) > 4:
+    to_display = '0000'
+
+
+def combine_horizontally(n1, n2):
+    n1 = int(n1)
+    n2 = int(n2)
+    first_num_pixels = np.array(all_images[n1]).tolist()
+    second_num_pixels = np.array(all_images[n2]).tolist()
+
+    new_image_pixels = []
+
+    for y in range(len(first_num_pixels)):
+        row = first_num_pixels[y]
+        while len(row) < 8:
+            row.append([0, 0, 0, 255])
+
+        for x in range(8):
+            row2 = second_num_pixels[y]
+            while len(row2) < 8:
+                row2.insert(0, [0,0,0,255])
+
+            row.append(row2[x])
+
+        new_image_pixels.append(np.uint8(row))
+
+    # im = PIL.Image.fromarray(np.array(new_image_pixels))
+    # im.show()
+    return new_image_pixels
+
+
+top = combine_horizontally(to_display[0], to_display[1])
+bottom = combine_horizontally(to_display[2], to_display[3])
+
+
+def combine_vertically(top, bottom):
+    all_px = top
+
+    while len(top) < 8:
+        newrow = [[0,0,0,255]] * 16
+        newrow = np.uint8(newrow)
+        top.append(newrow)
+
+    while len(bottom) < 8:
+        newrow = [[0,0,0,255]] * 16
+        newrow = np.uint8(newrow)
+        bottom.insert(0, newrow)
+
+    for y in range(len(bottom)):
+        all_px.append(bottom[y])
+
+    all_px = all_px
+    return all_px
+
+combined = combine_vertically(top, bottom)
+im = PIL.Image.fromarray(np.array(combined))
+im.show()
--- src/connect.py
+++
@@ -1,3 +0,0 @@
-import ip
-import time
-
