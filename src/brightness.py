e168b60eed16ae431382141a54ecec5903487fb0
amhenikoff <addison@henikoff.com>
2020-09-17 23:38:00 -0500

    Update brightness.py

--- src/brightness.py
+++ src/brightness.py
@@ -2,7 +2,7 @@ def get_output_brightness(user_brightness, sensor_value, max_brightness, min_bri
 	if sensor_value < 0:
             sensor_value = 0
 			
-	interpreted = sensor_value / 45
+	interpreted = sensor_value / 20
 
 	if interpreted > 1:
 		interpreted = 1
