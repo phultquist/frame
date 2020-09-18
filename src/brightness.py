100162e51cf88e1cc939739b9d79a394eb3b8065
amhenikoff <addison@henikoff.com>
2020-09-17 23:25:31 -0500

    Update brightness.py

--- src/brightness.py
+++ src/brightness.py
@@ -7,10 +7,10 @@ def get_output_brightness(user_brightness, sensor_value, max_brightness, min_bri
 	if interpreted > 1:
 		interpreted = 1
 
-	# just average the 2
-	output_brightness = (interpreted + user_brightness) / 2
+	# multiply interpreted by linear user_brightness value from 0 to 2
+	output_brightness = (interpreted * user_brightness) / 50
 
 	# this is some version of point slope form
-	output_brightness = output_brightness * (max_brightness - min_brightness) + min_brightness
+	#output_brightness = output_brightness * (max_brightness - min_brightness) + min_brightness
 
 	return output_brightness
