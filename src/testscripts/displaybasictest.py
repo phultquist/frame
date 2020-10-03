3300ff9a47e605c21125780a8c0c5c8a7ada03b3
phultquist <phultquist@imsa.edu>
2020-10-03 00:52:13 -0500

    bascitest

--- src/testscripts/displaybasictest.py
+++ src/testscripts/displaybasictest.py
@@ -1,11 +1,6 @@
+
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D12, 256)
+pixels = neopixel.NeoPixel(board.D12, 30)
 
-j = 0
-step = 256
-
-# for the life of me, i have no idea why this has to be a loop. i tried pixels = mbdtf and every time it showed funky colors, so here we are
-while j < len(pixels) + 1:
-    pixels[j - step:j] = (255,255,255)
-    j += step
+pixels[0] = (255, 0, 0)
