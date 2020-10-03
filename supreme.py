7950fae5ca79b46198ddf1ae392b63b091d95703
phultquist <phultquist@imsa.edu>
2020-10-03 01:56:53 -0500

    rgb

--- supreme.py
+++ supreme.py
@@ -1,5 +1,11 @@
 import subprocess
 import threading
+import board
+import neopixel
+
+pixels = neopixel.NeoPixel(board.D12, 30)
+
+pixels[2] = (0, 0, 255)
 
 def server():
     subprocess.call(['bash', 'server.sh'])
