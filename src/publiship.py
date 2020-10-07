259556af1d2ab823f88ee2d15a13122ee8e81046
phultquist <phultquist@imsa.edu>
2020-10-06 21:05:52 -0500

    publiship seperate file

---
+++ src/publiship.py
@@ -0,0 +1,27 @@
+import ip
+import requests
+from secrets import FRAME_ID
+
+stored_ip_address = ''
+
+def send_ip_to_server():
+    global stored_ip_address
+    #the ip is used in the app
+    latest_ip_address = ip.get_ip_address()
+    if not (latest_ip_address == stored_ip_address):
+        ip_address_url = "http://"+latest_ip_address+":3000/"
+
+        # the code for this is not open source. It basically just stores the global and local ip
+        r = requests.get("https://patrick.today/frame/set", params={'ip': ip_address_url, 'frameId': FRAME_ID})
+        # print(r.url)
+        if r.status_code == 200:
+            # successful update
+            print("global ip updated")
+            pass
+        else:
+            print(r)
+            print("there was an error updating the global ip")
+        
+        stored_ip_address = latest_ip_address
+
+send_ip_to_server()
--- src/run.py
+++ src/run.py
@@ -7,14 +7,11 @@ import schedule
 import threading
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import exceptions
-import ip
-import requests
-from secrets import FRAME_ID
+import publiship
 
 # initializes the last song so that way there is no error
 last_song = exceptions.exc_object('off')
 num_runs = 0
-stored_ip_address = ''
 
 def job():
     global last_song
@@ -24,25 +21,7 @@ def job():
     last_song = index.main(last_song.get('image_url'))
 
 def send_ip_to_server():
-    global stored_ip_address
-    #the ip is used in the app
-    latest_ip_address = ip.get_ip_address()
-    if not (latest_ip_address == stored_ip_address):
-        ip_address_url = "http://"+latest_ip_address+":3000/"
-
-        # the code for this is not open source. It basically just stores the global and local ip
-        r = requests.get("https://patrick.today/frame/set", params={'ip': ip_address_url, 'frameId': FRAME_ID})
-        # print(r.url)
-        if r.status_code == 200:
-            # successful update
-            print("global ip updated")
-            pass
-        else:
-            print(r)
-            print("there was an error updating the global ip")
-        
-        stored_ip_address = latest_ip_address
-    # requests.get()
+    publiship.send_ip_to_server()
 
 refresh_interval = 0.5 # seconds per refresh
 schedule.every(refresh_interval).seconds.do(job)
@@ -50,7 +29,6 @@ schedule.every(refresh_interval).seconds.do(job)
 schedule.every(1).minutes.do(send_ip_to_server)
 
 def music():
-    send_ip_to_server()
     while 1:
         schedule.run_pending()
 
